b03c817a140aa038291136f685bdd01119444032
Sergey Rukhlov <rukhlov-s@mail.ru>
2022-06-28 23:10:52 +0300

    Домашняя работа, задача №2

---
+++ Task4/MyProgram.cs
@@ -0,0 +1,29 @@
+﻿namespace Lessons
+{
+
+        // Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+        //4 -> да
+        //3 -> нет
+        //7 -> нет
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int a;
+                
+                Console.WriteLine("Введите число");
+
+                a = int.Parse(Console.ReadLine());
+                if(a % 2 == 0)
+                {
+                Console.WriteLine("Четное число");
+                }
+                 else if(a % 2 == 1)
+                {
+                Console.WriteLine("Нечетное число");
+                }
+
+
+            }
+        }
+    }
